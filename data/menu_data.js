9ad030d4c685cb70dfbb0011a26439cc3a6ee122
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-07 16:22:14 +0900

    Add menu data for jiajia and dahuchun

---
+++ data/menu_data.js
@@ -0,0 +1,13 @@
+window.menuData = {
+    jiajia: {
+        "면류": [{cn:"雪菜肉丝面", py:"xuěcài ròusī miàn", kr:"갓절임 돼지고기 면", price:"18"}, {cn:"葱油拌面", py:"cōngyóu bànmiàn", kr:"파기름 비빔면", price:"12"}],
+        "요리": [{cn:"红烧咸蛋黄狮子头", py:"hóngshāo xiándànhuáng shīzitóu", kr:"중국식 오리알 미트볼", price:"14"}],
+        "탕류": [{cn:"满馆单档汤", py:"mǎnguǎn dāndàng tāng", kr:"단당 완탕", price:"13"}],
+        "소룡포": [{cn:"经典鲜肉汤包", py:"jīngdiǎn xiānròu tāngbāo", kr:"기본 돼지고기 소룡포", price:"23"}]
+    },
+    dahuchun: {
+        "생전": [{cn:"鲜肉生煎", py:"xiānròu shēngjiān", kr:"돼지고기 구운 만두", price:"11"}],
+        "완탕": [{cn:"虾肉小馄饨", py:"xiāròu xiǎohúntún", kr:"새우 작은 완탕", price:"12"}],
+        "일품": [{cn:"老上海炸猪排+年糕", py:"lǎoshànghǎi zhàzhūpái + niángāo", kr:"돈가스+떡", price:"20"}]
+    }
+};
