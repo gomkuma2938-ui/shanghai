dd5d76000608fd6f0a47cdd90a6c02d6d3da207b
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-08 16:17:07 +0900

    Expand haidilao menu with new items and categories
    Added new menu items and categories for haidilao, including combinations and various dishes.

--- data/menu_data.js
+++ data/menu_data.js
@@ -84,24 +84,131 @@ window.menuData = {
             { cn: "百威", py: "bǎiwei", kr_read: "바이웨이", kr: "버드와이저 (330ml)", price: "6" }
         ]
     },
-    haidilao: {
-        "탕류(육수)": [
-            { cn: "清油麻辣火锅", py: "qīngyóu málà huǒguō", kr_read: "칭요우 마라 훠궈", kr: "청유 마라탕 (기본 매운맛)", price: "30~" },
-            { cn: "番茄火锅", py: "fānqié huǒguō", kr_read: "판치에 훠궈", kr: "토마토탕 (강추)", price: "28~" },
-            { cn: "菌汤火锅", py: "jūntāng huǒguō", kr_read: "쥔탕 훠궈", kr: "버섯탕", price: "25~" },
-            { cn: "猪肚鸡火锅", py: "zhūdùjī huǒguō", kr_read: "쭈뚜지 훠궈", kr: "삼계탕맛(돼지위+닭)", price: "32~" }
+haidilao: {
+        "가성비 조합": [
+            { cn: "毛肚虾滑肉肉组合", py: "máodù xiāhuá ròuròu zǔhé", kr_read: "마오두 시아화 로우로우 주허", kr: "천엽·새우완자·고기 모둠", price: "143" },
+            { cn: "鲜蔬菜品组合", py: "xiān shūcài pǐn zǔhé", kr_read: "시엔 슈차이 핀 주허", kr: "신선 채소 모둠", price: "48.8" },
+            { cn: "牛羊鱼虾肉肉组合", py: "niúyángyúxiā ròuròu zǔhé", kr_read: "니우양위시아 로우로우 주허", kr: "소고기·양고기·생선·새우 모둠", price: "166" }
         ],
-        "고기류": [
-            { cn: "捞派肥牛", py: "lāopài féiniú", kr_read: "라오파이 페이뉴", kr: "하이디라오 특제 소고기", price: "68" },
-            { cn: "捞派羊肉", py: "lāopài yángròu", kr_read: "라오파이 양로우", kr: "하이디라오 특제 양고기", price: "62" },
-            { cn: "精品肥牛", py: "jīngpǐn féiniú", kr_read: "징핀 페이뉴", kr: "우삼겹", price: "58" },
-            { cn: "招牌虾滑", py: "zhāopái xiāhuá", kr_read: "쟈오파이 시아후아", kr: "새우 완자 (필수 주문)", price: "56" }
+        "가재 요리": [
+            { cn: "超级龙虾桶", py: "chāojí lóngxiā tǒng", kr_read: "차오지 롱시아 통", kr: "슈퍼 가재 버킷", price: "135" },
+            { cn: "麻麻辣辣小龙虾1份", py: "mámálàlà xiǎolóngxiā 1 fèn", kr_read: "마마라라 샤오롱시아 이펀", kr: "마라 민물가재 1인분", price: "62.8" },
+            { cn: "麻麻辣辣小龙虾2份", py: "mámálàlà xiǎolóngxiā 2 fèn", kr_read: "마마라라 샤오롱시아 얼펀", kr: "마라 민물가재 2인분", price: "125.8" },
+            { cn: "蒜蓉小龙虾1份", py: "suànróng xiǎolóngxiā 1 fèn", kr_read: "쑤안롱 샤오롱시아 이펀", kr: "마늘향 민물가재 1인분", price: "62.8" },
+            { cn: "小龙虾自选双拼", py: "xiǎolóngxiā zìxuǎn shuāngpīn", kr_read: "샤오롱시아 즈쉬안 슈앙핀", kr: "민물가재 셀프 선택 두 가지 맛", price: "125.8" }
         ],
-        "야채/사이드": [
-            { cn: "蔬菜拼盘", py: "shūcài pīnpán", kr_read: "슈차이 핀판", kr: "모둠 채소", price: "38" },
-            { cn: "菌类拼盘", py: "jùnlèi pīnpán", kr_read: "쥔레이 핀판", kr: "모둠 버섯", price: "36" },
-            { cn: "捞派捞面", py: "lāopài lāomiàn", kr_read: "라오파이 라오미엔", kr: "수제 수타면 (퍼포먼스 면)", price: "7" },
-            { cn: "自助调料", py: "zìzhù tiáoliào", kr_read: "쯔주 티아오리아오", kr: "소스바 이용료 (1인당)", price: "10" }
+        "특선 훠궈 탕": [
+            { cn: "三鲜火锅", py: "sānxiān huǒguō", kr_read: "산시엔 후오구오", kr: "삼선 훠궈", price: "31" },
+            { cn: "清油麻辣火锅", py: "qīngyóu málà huǒguō", kr_read: "칭요우 마라 후오구오", kr: "맑은 기름 마라 훠궈", price: "31" },
+            { cn: "海底捞新一代番茄火锅", py: "hǎidǐlāo xīnyīdài fānqié huǒguō", kr_read: "하이디라오 신세대 토마토 훠궈", kr: "하이디라오 신세대 토마토 훠궈", price: "33" },
+            { cn: "经典麻辣火锅 (牛油风味)", py: "jīngdiǎn málà huǒguō (niúyóu fēngwèi)", kr_read: "징디엔 마라 후오구오 (니우요우 펑웨이)", kr: "클래식 마라 훠궈 (우지 맛)", price: "36" },
+            { cn: "鸳鸯火锅底料 (适用6升鸳鸯锅)", py: "yuānyāng huǒguō dǐliào (shìyòng 6 shēng yuānyāng guō)", kr_read: "유앤양 후오구오 디리야오 (쓰용 리우셩 유앤양 구오)", kr: "원앙 훠궈 육수 (6L 원앙냄비용)", price: "62" },
+            { cn: "苗族酸汤春春锅", py: "miáozú suāntāng chūnchūn guō", kr_read: "미아오주 쑤안탕 춘춘 구오", kr: "묘족 쏸탕(신맛 육수) 춘춘 훠궈", price: "28" },
+            { cn: "浓浓浓菌汤火锅", py: "nóngnóngnóng jūntāng huǒguō", kr_read: "농농농 쥔탕 후오구오", kr: "진한 버섯 훠궈", price: "32" }
+        ],
+        "소/양고기": [
+            { cn: "捞派滑牛肉", py: "lāopài huá niúròu", kr_read: "라오파이 화 니우로우", kr: "라오파이 슬라이스 소고기", price: "38" },
+            { cn: "内蒙草原羔羊肉", py: "nèiměng cǎoyuán gāoyángròu", kr_read: "네이멍 차오유앤 까오양로우", kr: "내몽고 초원 양고기", price: "33" },
+            { cn: "捞派肥牛", py: "lāopài féiniú", kr_read: "라오파이 페이니우", kr: "라오파이 차돌양지", price: "39" },
+            { cn: "火锅牛排", py: "huǒguō niúpái", kr_read: "후오구오 니우파이", kr: "훠궈용 소고기 스테이크", price: "34" },
+            { cn: "内蒙古特选肥牛", py: "nèiměnggǔ tèxuǎn féiniú", kr_read: "네이멍구 터쉬앤 페이니우", kr: "내몽고 특선 차돌양지", price: "36" },
+            { cn: "“嘎嘎香” 千层毛肚", py: "“gāgāxiāng” qiāncéng máodù", kr_read: "가아가샹 치앤청 마오두", kr: "\"가아가향\" 천엽", price: "44" },
+            { cn: "木姜子十秒牛肉", py: "mùjiāngzǐ shí miǎo niúròu", kr_read: "무지앙즈 쓰미아오 니우로우", kr: "목강자 10초 소고기", price: "38" }
+        ],
+        "완자/해산물": [
+            { cn: "捞派巴沙鱼片", py: "lāopài bāshāyúpiàn", kr_read: "라오파이 바샤위피엔", kr: "라오파이 메기 슬라이스", price: "32" },
+            { cn: "招牌大颗粒虾滑", py: "zhāopái dà kēlì xiāhuá", kr_read: "자오파이 따 커리 시아화", kr: "시그니처 새우 완자", price: "38" },
+            { cn: "Q弹劲道牛肉丸", py: "Q tán jìndào niúròu wán", kr_read: "큐 탄 찐따오 니우로우 완", kr: "탱글탱글 소고기 완자", price: "32" },
+            { cn: "脆骨鱼豆腐", py: "cuìgǔ yú dòufu", kr_read: "췌이구 위 도우푸", kr: "연골 어묵 두부", price: "16" },
+            { cn: "鳕鱼蟹味棒", py: "xuěyú xièwèi bàng", kr_read: "쉐위 시에웨이 빵", kr: "대구 게맛살", price: "22" },
+            { cn: "嫩滑黑鱼片", py: "nèn huá hēiyú piàn", kr_read: "넌 화 헤이위 피엔", kr: "부드러운 가물치 슬라이스", price: "25" },
+            { cn: "虾仁福袋", py: "xiārén fúdài", kr_read: "시아런 푸따이", kr: "새우 복주머니 어묵", price: "24" },
+            { cn: "V形蟹味柳", py: "V xíng xièwèi liǔ", kr_read: "V싱 시에웨이 리우", kr: "V자 게맛살", price: "16" }
+        ],
+        "기타 육류": [
+            { cn: "捞派鴨腸", py: "lāopài yācháng", kr_read: "라오파이 야창", kr: "라오파이 오리 창자", price: "27" },
+            { cn: "捞派猪黄喉", py: "lāopài zhū huánghóu", kr_read: "쭈 후앙호우", kr: "라오파이 돼지 혈관", price: "34" },
+            { cn: "층층설랑저오화", py: "céngcéng xuělàng zhū wǔhuā", kr_read: "청청슈에랑 쭈 우화", kr: "겹겹이 눈 같은 돼지 삼겹살", price: "25" },
+            { cn: "我的小郡肝", py: "wǒ de xiǎo jùngān", kr_read: "워 더 샤오 쥔간", kr: "나의 닭모래집 꼬치", price: "24" },
+            { cn: "捞派毛肚", py: "lāopài máodù", kr_read: "라오파이 마오두", kr: "라오파이 천엽", price: "44" },
+            { cn: "捞派脆脆毛肚", py: "lāopài cuìcuì máodù", kr_read: "라오파이 췌이췌이 마오두", kr: "라오파이 아삭한 천엽", price: "44" },
+            { cn: "脱骨鸭掌", py: "tuōgǔ yāzhǎng", kr_read: "투오구 야장", kr: "뼈 없는 오리 발", price: "33" },
+            { cn: "鸭舌", py: "yāshé", kr_read: "야서", kr: "오리 혀", price: "31" },
+            { cn: "鹌鹑蛋", py: "ānchúndàn", kr_read: "안춘단", kr: "메추리알", price: "19" },
+            { cn: "小香肠", py: "xiǎo xiāngcháng", kr_read: "샤오 시앙창", kr: "미니 소시지", price: "18" },
+            { cn: "川味果香五花腊肉", py: "chuānwèi guǒxiāng wǔhuā làròu", kr_read: "츄앤웨이 구오샹 우화 라로우", kr: "사천식 과일향 훈제 삼겹살", price: "23" },
+            { cn: "瀑布雪花猪肉片", py: "pùbù xuěhuā zhūròupiàn", kr_read: "푸뿌 슈에화 쭈로우피엔", kr: "폭포 눈꽃 돼지고기 슬라이스", price: "26" }
+        ],
+        "채소/버섯": [
+            { cn: "8秒海带苗", py: "8 miǎo hǎidàimiáo", kr_read: "빠미아오 하이따이미아오", kr: "8초 미역 순", price: "14" },
+            { cn: "海底捞1号沙沙土豆", py: "Hǎidǐlāo 1 hào shāshā tǔdòu", kr_read: "하이디라오 이호 샤샤 투도우", kr: "하이디라오 1호 포슬포슬 감자", price: "10" },
+            { cn: "竹笋", py: "zhúsǔn", kr_read: "주쑨", kr: "죽순", price: "17" },
+            { cn: "金色金针菇", py: "jīnsè jīnzhēngū", kr_read: "진써 진쩐구", kr: "황금 팽이버섯", price: "19" },
+            { cn: "铁棍山药", py: "tiěgùn shānyào", kr_read: "티에군 샨야오", kr: "철곤 마", price: "17" },
+            { cn: "香菇", py: "xiānggū", kr_read: "시앙구", kr: "표고버섯", price: "11" },
+            { cn: "干贡菜", py: "gān gòngcài", kr_read: "간 꽁차이", kr: "말린 궁채", price: "17" },
+            { cn: "白萝卜", py: "báiluóbo", kr_read: "바이뤄보", kr: "무", price: "7" },
+            { cn: "高山娃娃菜(有机认证原料)", py: "gāoshān wáwácài", kr_read: "까오샨 와와차이", kr: "고산 알배기 배추(유기농 인증 원료)", price: "11" },
+            { cn: "冬瓜*", py: "dōngguā", kr_read: "동구아", kr: "동아", price: "9" },
+            { cn: "菠菜苗", py: "bōcài miáo", kr_read: "뽀차이 미아오", kr: "시금치 순", price: "11" }
+        ],
+        "두부/면류": [
+            { cn: "8秒劲道鲜油豆皮", py: "8 miǎo jìndào xiān yóudòupí", kr_read: "빠미아오 찐따오 시엔 요우도우피", kr: "8초 쫄깃 생유부피", price: "14" },
+            { cn: "3秒铃铃卷", py: "3 miǎo línglíng juǎn", kr_read: "싼미아오 링링쥐앤", kr: "3초 링링권(푸주 롤)", price: "15" },
+            { cn: "冻豆腐", py: "dòng dòufu", kr_read: "똥 도우푸", kr: "언 두부", price: "13" },
+            { cn: "捞派魔芋素 “毛肚”", py: "lāopài móyù sù “máodù”", kr_read: "라오파이 모위 쑤 마오두", kr: "라오파이 곤약 \"천엽\"", price: "11" },
+            { cn: "水晶粉丝", py: "shuǐjīng fěnsī", kr_read: "슈에이징 펀쓰", kr: "수정 당면", price: "10" },
+            { cn: "注心火锅油条 (咸蛋黄味)", py: "zhùxīn huǒguō yóutiáo (xiándànhuáng wèi)", kr_read: "쭈신 후오구오 요우티아오", kr: "속을 채운 훠궈 요우티아오 (염장 달걀 노른자 맛)", price: "17" },
+            { cn: "豆腐皮", py: "dòufu pí", kr_read: "도우푸 피", kr: "두부피", price: "11" },
+            { cn: "捞派豆花", py: "lāopài dòuhuā", kr_read: "라오파이 도우화", kr: "라오파이 연두부(또우화)", price: "18" },
+            { cn: "6秒腐竹", py: "6 miǎo fǔzhú", kr_read: "류미아오 푸주", kr: "6초 푸주", price: "14" },
+            { cn: "捞派捞面", py: "lāopài lāomiàn", kr_read: "라오파이 라오미엔", kr: "하이디라오 쿵푸 수제면", price: "" }
+        ],
+        "간식/주식": [
+            { cn: "五常米饭", py: "wǔcháng mǐfàn", kr_read: "우창 미판", kr: "우창 쌀밥", price: "3" },
+            { cn: "酥酥嫩嫩炸牛奶", py: "sūsū nènnèn zhà niúnǎi", kr_read: "쑤쑤 넌넌 쨔 니우나이", kr: "바삭하고 부드러운 튀긴 우유", price: "12" },
+            { cn: "现炸酥肉", py: "xiànzhà sūròu", kr_read: "시엔쨔 쑤로우", kr: "즉석 튀김 돼지고기", price: "27" },
+            { cn: "生面条", py: "shēng miàntiáo", kr_read: "셩 미앤티아오", kr: "생면", price: "5" },
+            { cn: "一口爆浆奶酪鱼条", py: "yīkǒu bàojiāng nǎilào yútiáo", kr_read: "이코우 빠오지앙 나이라오 위티아오", kr: "한입 치즈 폭탄 생선 스틱", price: "16" },
+            { cn: "胖胖哒糍粑", py: "pàngpàngdā cíbā", kr_read: "팡팡다 치바", kr: "촉촉한 중국식 찹쌀떡 (인절미 튀김)", price: "" }
+        ],
+        "주류/음료": [
+            { cn: "100%HPP金堂橙汁", py: "100% HPP Jīntáng chéngzhī", kr_read: "100% HPP 진탕 청즈", kr: "100% HPP 진탕 오렌지 주스", price: "12.9" },
+            { cn: "冷萃兰韵乌龙茶", py: "lěngcuì lányùn wūlóngchá", kr_read: "렁췌이 란윈 우롱차", kr: "콜드브루 난운 우롱차", price: "8.9" },
+            { cn: "100%富士苹果汁", py: "100% fùshì píngguǒzhī", kr_read: "100% 푸스 핑구오즈", kr: "100% 후지 사과 주스", price: "12.9" },
+            { cn: "杨枝甘露", py: "yángzhīgānlù", kr_read: "양즈간루", kr: "양지감로", price: "12.9" },
+            { cn: "40朵茉莉轻乳茶", py: "40 duǒ mòlì qīngrǔchá", kr_read: "40둬 모리 칭루차", kr: "40송이 재스민 밀크티", price: "18" },
+            { cn: "100%HPP西瓜汁", py: "100% HPP xīguāzhī", kr_read: "100% HPP 시구아즈", kr: "100% HPP 수박 주스", price: "12.9" },
+            { cn: "可口可乐1.25L", py: "kěkǒukělè 1.25L", kr_read: "커코우커러 1.25L", kr: "코카콜라 1.25L", price: "15" },
+            { cn: "听可乐", py: "tīng kělè", kr_read: "팅 커러", kr: "캔 콜라", price: "8" },
+            { cn: "零度可口可乐330mL", py: "língdù kěkǒukělè 330mL", kr_read: "링두 커코우커러 330mL", kr: "코카콜라 제로 330mL", price: "8" },
+            { cn: "海底捞大麦拉格啤酒500ml", py: "hǎidǐlāo dàmài lāgé píjiǔ", kr_read: "하이디라오 다마이 라거 피지우", kr: "하이디라오 보리 라거 맥주 500ml", price: "13" },
+            { cn: "海底捞德式小麦啤酒500ml", py: "hǎidǐlāo déshì xiǎomài píjiǔ", kr_read: "하이디라오 더스 시아오마이 피지우", kr: "하이디라오 독일식 밀맥주 500ml", price: "13" },
+            { cn: "海底捞淡爽拉格啤酒500ml", py: "hǎidǐlāo dànshuǎng lāgé píjiǔ", kr_read: "하이디라오 딴슈앙 라거 피지우", kr: "하이디라오 라이트 라거 맥주 500ml", price: "8" },
+            { cn: "王老吉(听装)", py: "wánglǎojí (tīngzhuāng)", kr_read: "왕라오지 (팅좡)", kr: "왕라오지 (캔)", price: "8" },
+            { cn: "雪碧330ml", py: "xuěbì 330ml", kr_read: "쉬에비 330ml", kr: "스프라이트 330ml", price: "8" },
+            { cn: "海底捞原浆白啤300ml", py: "hǎidǐlāo yuánjiāng báipí 300ml", kr_read: "하이디라오 위엔지앙 바이피 300ml", kr: "하이디라오 원장 밀맥주 300ml", price: "12" },
+            { cn: "雪花纯生500ml", py: "xuěhuā chúnshēng 500ml", kr_read: "쉬에화 춘셩 500ml", kr: "설화 순생 맥주 500ml", price: "" },
+            { cn: "椰树牌椰汁310mL", py: "yēshùpái yēzhī 310mL", kr_read: "예슈파이 예즈 310ml", kr: "예수패 코코넛 주스 310ml", price: "" }
+        ],
+        "소스": [
+            { cn: "丸滑调味汁", py: "wánhuá tiáowèizhī", kr_read: "완화 티아오웨이즈", kr: "완자용 조미 소스", price: "1" },
+            { cn: "海椒干碟", py: "hǎijiāo gāndié", kr_read: "하이지아오 간디에", kr: "고추 건식 소스", price: "1" },
+            { cn: "牛肉末", py: "niúròu mò", kr_read: "니우로우 모", kr: "다진 소고기", price: "10" },
+            { cn: "香油蒜泥味碟", py: "xiāngyóu suànní wèidié", kr_read: "시앙요우 쑤안니 웨이디에", kr: "참기름 다진 마늘 소스", price: "10" },
+            { cn: "花生碎", py: "huāshēng suì", kr_read: "후아셩 쑤이", kr: "땅콩 분태", price: "1" },
+            { cn: "花生芝麻调味酱", py: "huāshēng zhīmá tiáowèijiàng", kr_read: "후아셩 즈마 티아오웨이지앙", kr: "땅콩 깨 소스", price: "10" },
+            { cn: "蒜蓉辣椒酱", py: "suànróng làjiāojiàng", kr_read: "쑤안롱 라지아오지앙", kr: "마늘 고추 소스", price: "3" },
+            { cn: "蚝油", py: "háoyóu", kr_read: "하오요우", kr: "굴소스", price: "1" },
+            { cn: "醋", py: "cù", kr_read: "추", kr: "식초", price: "1" },
+            { cn: "香菜末", py: "xiāngcài mò", kr_read: "시앙차이 모", kr: "고수 다짐", price: "1" },
+            { cn: "葱花", py: "cōnghuā", kr_read: "총후아", kr: "대파 다짐", price: "1" },
+            { cn: "小米辣", py: "xiǎomǐlà", kr_read: "샤오미라", kr: "중국 매운 고추", price: "2" },
+            { cn: "芹菜末", py: "qíncài mò", kr_read: "친차이 모", kr: "셀러리 다짐", price: "1" },
+            { cn: "瑶柱香菇酱", py: "yáozhù xiānggūjiàng", kr_read: "야오주 시앙구지앙", kr: "관자 표고버섯 소스", price: "8" },
+            { cn: "沙茶酱", py: "shāchájiàng", kr_read: "샤차지앙", kr: "사차장(해물+견과류 소스)", price: "8" },
+            { cn: "精品牛肉酱", py: "jīngpǐn niúròujiàng", kr_read: "징핀 니우로우지앙", kr: "명품 소고기 소스", price: "8" },
+            { cn: "蒜泥", py: "suànní", kr_read: "쑤안니", kr: "다진 마늘", price: "1" }       
         ]
     },
     jiangbian: {
