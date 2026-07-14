493761699e30e1b67a639108d059a4099f19aab9
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-14 17:54:55 +0900

    Update talk.js

--- data/talk.js
+++ data/talk.js
@@ -11,13 +11,20 @@ window.talkData = {
         { cn: "我要这个", py: "wǒ yào zhège", kr_read: "워 야오 저거", kr: "이거 하나 주세요" },
         { cn: "点单", py: "diǎndān", kr_read: "디앤딴", kr: "주문할게요" },
         { cn: "可以打包吗?", py: "kěyǐ dǎbāo ma?", kr_read: "커이 따바오 마?", kr: "포장 가능한가요?" },
-        { cn: "不辣的", py: "bùlàde", kr_read: "부라더", kr: "안 맵게 해주세요" }
+        { cn: "不辣的", py: "bùlàde", kr_read: "부라더", kr: "안 맵게 해주세요" },
+        { cn: "四位", py: "sì wèi", kr_read: "쓰 웨이", kr: "4명입니다" },
+        { cn: "我要用优惠券", py: "wǒ yào yòng yōuhuìquàn", kr_read: "워 야오 용 요우후이추엔", kr: "쿠폰 쓸게요" },
+        { cn: "我要用代金券", py: "wǒ yào yòng dàijīnquàn", kr_read: "워 야오 용 따이진추엔", kr: "대금권 쓸게요" }
     ],
     "교통/이동": [
         { cn: "去这里", py: "qù zhèlǐ", kr_read: "취 쩌리", kr: "여기로 가주세요" },
         { cn: "这里停一下", py: "zhèlǐ tíng yíxià", kr_read: "쩌리 팅 이시아", kr: "여기서 세워주세요" },
         { cn: "打开后备箱", py: "dǎkāi hòubèixiāng", kr_read: "따카이 허우뻬이샹", kr: "트렁크 좀 열어주세요" },
         { cn: "洗手间在哪里?", py: "xǐshǒujiān zài nǎlǐ?", kr_read: "시쇼우지앤 짜이 나리?", kr: "화장실 어디예요?" }
+    ],
+    "관광지/입장": [
+        { cn: "买四张成人票", py: "mǎi sì zhāng chéngrén piào", kr_read: "마이 쓰 짱 청런 피아오", kr: "어른 4명 표 주세요" },
+        { cn: "三张成人票, 一张老人票", py: "sān zhāng chéngrén piào, yī zhāng lǎorén piào", kr_read: "산 짱 청런 피아오, 이 짱 라오런 피아오", kr: "성인 3장, 경로 1장 주세요" }
     ],
     "쇼핑/기타": [
         { cn: "多少钱?", py: "duōshǎo qián?", kr_read: "뚜오샤오 치앤?", kr: "얼마예요?" },
