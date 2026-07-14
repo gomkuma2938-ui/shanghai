83d699e072b6d0aa153ea1ea10482681b740739f
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-14 21:31:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,35 @@
-const CACHE_NAME = 'shanghai-v1';
+const CACHE_NAME = 'shanghai-v2'; // 데이터 바뀔 때마다 버전 숫자 올리기
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './data/hotel.js',
-  './data/tour.js',
-  './data/restaurant.js',
+  './data/location.js',
   './data/menu_data.js',
   './data/talk.js',
   './data/schedule.js'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, res.clone()));
+        return res.clone();
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
