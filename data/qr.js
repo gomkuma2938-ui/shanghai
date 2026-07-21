5692ff802db5de34512136ba14dac56d800fb67d
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-21 17:15:55 +0900

    Create qr.js

---
+++ data/qr.js
@@ -0,0 +1,11 @@
+window.qrData = [
+    { 
+        kr: '디즈니랜드 입장 QR', 
+        addr: '디즈니랜드 정문', 
+        qrs: [
+            { name: '홍길동', src: 'images/qr_disney_1.png' },
+            { name: '김철수', src: 'images/qr_disney_2.png' }
+        ],
+        desc: "여권과 함께 제시하세요."
+    }
+];
