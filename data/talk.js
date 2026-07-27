33c71c04c2336becd1ba509cfe4df2d409894f8b
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-27 09:10:15 +0900

    Update talk.js

--- data/talk.js
+++ data/talk.js
@@ -4,7 +4,11 @@ window.talkData = {
         { cn: "谢谢", py: "xièxie", kr_read: "씨에씨에", kr: "감사합니다" },
         { cn: "对不起", py: "duìbuqǐ", kr_read: "뚜이부치", kr: "죄송합니다" },
         { cn: "好的", py: "hǎode", kr_read: "하오더", kr: "좋아요 / 알겠습니다" },
-        { cn: "不用了", py: "búyòngle", kr_read: "부용러", kr: "괜찮습니다 / 필요없어요" }
+        { cn: "不用了", py: "búyòngle", kr_read: "부용러", kr: "괜찮습니다 / 필요없어요" },
+        { cn: "请帮帮我", py: "qǐng bāngbang wǒ", kr_read: "칭 방방 워", kr: "좀 도와주세요" },
+        { cn: "需要袋子", py: "xūyào dàizi", kr_read: "쉬야오 따이지", kr: "봉투 주세요" },
+        { cn: "请给我冰水", py: "qǐng gěi wǒ bīngshuǐ", kr_read: "칭 게이 워 빙슈이", kr: "차가운 물 주세요" },
+        { cn: "有纸巾吗?", py: "yǒu zhǐjīn ma?", kr_read: "요우 지진 마?", kr: "휴지 있나요?" }
     ],
     "호텔/숙박": [
         { cn: "我有预订", py: "wǒ yǒu yùdìng", kr_read: "워 요우 위딩", kr: "예약했습니다" },
@@ -20,13 +24,17 @@ window.talkData = {
     "식당/주문": [
         { cn: "不要香菜", py: "búyào xiāngcài", kr_read: "부야오 샹차이", kr: "고수 빼주세요" },
         { cn: "买单", py: "mǎidān", kr_read: "마이딴", kr: "계산할게요" },
+                { cn: "发票", py: "fāpiào", kr_read: "파피아오", kr: "영수증 끊어주세요" },
         { cn: "我要这个", py: "wǒ yào zhège", kr_read: "워 야오 저거", kr: "이거 하나 주세요" },
         { cn: "点单", py: "diǎndān", kr_read: "디앤딴", kr: "주문할게요" },
         { cn: "可以打包吗?", py: "kěyǐ dǎbāo ma?", kr_read: "커이 따바오 마?", kr: "포장 가능한가요?" },
         { cn: "不辣的", py: "bùlàde", kr_read: "부라더", kr: "안 맵게 해주세요" },
         { cn: "四位", py: "sì wèi", kr_read: "쓰 웨이", kr: "4명입니다" },
         { cn: "我要用优惠券", py: "wǒ yào yòng yōuhuìquàn", kr_read: "워 야오 용 요우후이추엔", kr: "쿠폰 쓸게요" },
-        { cn: "我要用代金券", py: "wǒ yào yòng dàijīnquàn", kr_read: "워 야오 용 따이진추엔", kr: "대금권 쓸게요" }
+        { cn: "我要用代金券", py: "wǒ yào yòng dàijīnquàn", kr_read: "워 야오 용 따이진추엔", kr: "대금권 쓸게요" },
+        { cn: "我扫你", py: "wǒ sǎo nǐ", kr_read: "워 사오 니", kr: "제가 QR 찍을게요" },
+        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" },
+        { cn: "可以付现金吗?", py: "kěyǐ fù xiànjīn ma?", kr_read: "커이 푸 시앤진 마?", kr: "현금 결제 되나요?" }
     ],
     "교통/이동": [
         { cn: "去这里", py: "qù zhèlǐ", kr_read: "취 쩌리", kr: "여기로 가주세요" },
@@ -41,6 +49,9 @@ window.talkData = {
     "쇼핑/기타": [
         { cn: "多少钱?", py: "duōshǎo qián?", kr_read: "뚜오샤오 치앤?", kr: "얼마예요?" },
         { cn: "太贵了, 便宜一点吧", py: "tài guì le, piányi yìdiǎn ba", kr_read: "타이꾸이러, 피엔이 이디앤바", kr: "너무 비싸요, 깎아주세요" },
-        { cn: "发票", py: "fāpiào", kr_read: "파피아오", kr: "영수증 끊어주세요" }
+        { cn: "发票", py: "fāpiào", kr_read: "파피아오", kr: "영수증 끊어주세요" },
+        { cn: "我扫你", py: "wǒ sǎo nǐ", kr_read: "워 사오 니", kr: "제가 QR 찍을게요" },
+        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" },
+        { cn: "可以付现金吗?", py: "kěyǐ fù xiànjīn ma?", kr_read: "커이 푸 시앤진 마?", kr: "현금 결제 되나요?" }
     ]
 };
