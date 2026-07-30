cad7334e2a123445b4c37ca0f33e0687290054b4
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-30 15:17:00 +0900

    Reorder phrases in talk.js for better structure

--- data/talk.js
+++ data/talk.js
@@ -3,9 +3,9 @@ window.talkData = {
         { cn: "不好意思", py: "bù hǎoyìsi", kr_read: "부하오이쓰", kr: "실례합니다 / 저기요" },
         { cn: "谢谢", py: "xièxie", kr_read: "씨에씨에", kr: "감사합니다" },
         { cn: "对不起", py: "duìbuqǐ", kr_read: "뚜이부치", kr: "죄송합니다" },
-        { cn: "我不会说中文", py: "wǒ búhuì shuō zhōngwén", kr_read: "워 부후이 슈오 쭝원", kr: "중국어 못해요" },
         { cn: "好的", py: "hǎode", kr_read: "하오더", kr: "좋아요 / 알겠습니다" },
         { cn: "不用了", py: "búyòngle", kr_read: "부용러", kr: "괜찮습니다 / 필요없어요" },
+        { cn: "我不会说中文", py: "wǒ búhuì shuō zhōngwén", kr_read: "워 부후이 슈오 쭝원", kr: "중국어 못해요" },
         { cn: "请帮帮我", py: "qǐng bāngbang wǒ", kr_read: "칭 방방 워", kr: "좀 도와주세요" },
         { cn: "需要袋子", py: "xūyào dàizi", kr_read: "쉬야오 따이지", kr: "봉투 주세요" },
         { cn: "请给我冰水", py: "qǐng gěi wǒ bīngshuǐ", kr_read: "칭 게이 워 빙슈이", kr: "차가운 물 주세요" },
@@ -18,6 +18,7 @@ window.talkData = {
         { cn: "可以寄存行李吗?", py: "kěyǐ jìcún xíngli ma?", kr_read: "커이 지츈 싱리 마?", kr: "짐 좀 맡길 수 있나요?" },
         { cn: "取行李", py: "qǔ xíngli", kr_read: "취 싱리", kr: "맡긴 짐 찾을게요" },
         { cn: "Wi-Fi密码是多少?", py: "Wi-Fi mìmǎ shì duōshǎo?", kr_read: "와이파이 미마 쓰 뚜오샤오?", kr: "와이파이 비밀번호가 뭐예요?" },
+        { cn: "请换一下床单", py: "qǐng huàn yíxià chuángdān", kr_read: "칭 후안 이시아 추앙단", kr: "침대 시트 좀 바꿔주세요" },
         { cn: "请打扫一下房间", py: "qǐng dǎsǎo yíxià fángjiān", kr_read: "칭 따사오 이시아 팡지앤", kr: "방 청소 좀 해주세요" },
         { cn: "没有热水", py: "méiyǒu rèshuǐ", kr_read: "메이요우 러슈이", kr: "따뜻한 물이 안 나와요" }
     ],
@@ -25,35 +26,49 @@ window.talkData = {
         { cn: "不要香菜", py: "búyào xiāngcài", kr_read: "부야오 샹차이", kr: "고수 빼주세요" },
         { cn: "买单", py: "mǎidān", kr_read: "마이딴", kr: "계산할게요" },
         { cn: "我要这个", py: "wǒ yào zhège", kr_read: "워 야오 저거", kr: "이거 하나 주세요" },
-        { cn: "一杯冰美式", py: "yì bēi bīng měishì", kr_read: "이 뻬이 빙 메이쓰", kr: "아이스 아메리카노 한 잔" },
-        { cn: "给我吸管", py: "gěi wǒ xīguǎn", kr_read: "게이 워 시꽌", kr: "빨대 주세요" },
+        { cn: "热的", py: "rède", kr_read: "러더", kr: "따뜻한 걸로 주세요" },
+        { cn: "少冰", py: "shǎo bīng", kr_read: "샤오 삥", kr: "얼음 적게 넣어주세요" },
         { cn: "点单", py: "diǎndān", kr_read: "디앤딴", kr: "주문할게요" },
         { cn: "不辣的", py: "bùlàde", kr_read: "부라더", kr: "안 맵게 해주세요" },
-        { cn: "有充电宝吗?", py: "yǒu chōngdiànbǎo ma?", kr_read: "요우 층띠앤바오 마?", kr: "보조배터리 대여 있나요?" },
         { cn: "可以打包吗?", py: "kěyǐ dǎbāo ma?", kr_read: "커이 따바오 마?", kr: "포장 가능한가요?" },
+        { cn: "有充电宝吗?", py: "yǒu chōngdiànbǎo ma?", kr_read: "요우 층띠앤바오 마?", kr: "보조배터리 대여 있나요?" },
         { cn: "四位", py: "sì wèi", kr_read: "쓰 웨이", kr: "4명입니다" },
+        { cn: "我要用优惠券", py: "wǒ yào yòng yōuhuìquàn", kr_read: "워 야오 용 요우후이추엔", kr: "쿠폰 쓸게요" },
+        { cn: "我要用代金券", py: "wǒ yào yòng dàijīnquàn", kr_read: "워 야오 용 따이진추엔", kr: "대금권 쓸게요" },
         { cn: "我扫你", py: "wǒ sǎo nǐ", kr_read: "워 사오 니", kr: "제가 QR 찍을게요" },
-        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" }
+        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" },
+        { cn: "可以付现金吗?", py: "kěyǐ fù xiànjīn ma?", kr_read: "커이 푸 시앤진 마?", kr: "현금 결제 되나요?" }
     ],
     "교통/이동": [
         { cn: "去这里", py: "qù zhèlǐ", kr_read: "취 쩌리", kr: "여기로 가주세요" },
         { cn: "我就在地图上的位置", py: "wǒ jiù zài dìtú shàng de wèizhì", kr_read: "워 찌우 짜이 띠투 샹더 웨이즈", kr: "지도상의 위치에 있어요" },
         { cn: "这里停一下", py: "zhèlǐ tíng yíxià", kr_read: "쩌리 팅 이시아", kr: "여기서 세워주세요" },
+        { cn: "入口在哪儿？", py: "rùkǒu zài nǎ'er?", kr_read: "루커우 짜이 나얼?", kr: "입구가 어디인가요?" },
+        { cn: "地铁站在哪儿？", py: "dìtiězhàn zài nǎ'er?", kr_read: "띠티에잔 짜이 나얼?", kr: "지하철역이 어디인가요?" },
         { cn: "打开后备箱", py: "dǎkāi hòubèixiāng", kr_read: "따카이 허우뻬이샹", kr: "트렁크 좀 열어주세요" },
-        { cn: "洗手间在哪里?", py: "xǐshǒujiān zài nǎlǐ?", kr_read: "시쇼우지앤 짜이 나리?", kr: "화장실 어디예요?" },
-        { cn: "药店在哪里?", py: "yàodiàn zài nǎlǐ?", kr_read: "야오띠앤 짜이 나리?", kr: "약국이 어디예요?" }
+        { cn: "洗手间在哪里?", py: "xǐshǒujiān zài nǎlǐ?", kr_read: "시쇼우지앤 짜이 나리?", kr: "화장실 어디예요?" }
     ],
     "관광지/입장": [
         { cn: "我有预订", py: "wǒ yǒu yùdìng", kr_read: "워 요우 위딩", kr: "예약했습니다" },
+        { cn: "帮 우리 拍张照吧", py: "bāng wǒmen pāi zhāng zhào ba", kr_read: "빵 워먼 파이 짱 쨔오바", kr: "저희 사진 좀 찍어주세요" },
         { cn: "买四张成人票", py: "mǎi sì zhāng chéngrén piào", kr_read: "마이 쓰 짱 청런 피아오", kr: "어른 4명 표 주세요" },
         { cn: "三张成人票, 一张老人票", py: "sān zhāng chéngrén piào, yī zhāng lǎorén piào", kr_read: "산 짱 청런 피아오, 이 짱 라오런 피아오", kr: "성인 3장, 경로 1장 주세요" }
     ],
     "쇼핑/기타": [
         { cn: "多少钱?", py: "duōshǎo qián?", kr_read: "뚜오샤오 치앤?", kr: "얼마예요?" },
+        { cn: "可以退税吗?", py: "kěyǐ tuìshuì ma?", kr_read: "커이 퇴이슈이 마?", kr: "택스 리펀(환급) 가능한가요?" },
+        { cn: "我要办退税", py: "wǒ yào bàn tuìshuì", kr_read: "워 야오 빤 퇴이슈이", kr: "택스 리펀(환급) 해주세요 / 신청할게요" },
+        { cn: "不要袋子", py: "búyào dàizi", kr_read: "부야오 따이지", kr: "봉투는 필요 없어요" },
         { cn: "太贵了, 便宜一点吧", py: "tài guì le, piányi yìdiǎn ba", kr_read: "타이꾸이러, 피엔이 이디앤바", kr: "너무 비싸요, 깎아주세요" },
         { cn: "发票", py: "fāpiào", kr_read: "파피아오", kr: "영수증 끊어주세요" },
-        { cn: "可以付现金吗?", py: "kěyǐ fù xiànjīn ma?", kr_read: "커이 푸 시앤진 마?", kr: "현금 결제 되나요?" },
         { cn: "我扫你", py: "wǒ sǎo nǐ", kr_read: "워 사오 니", kr: "제가 QR 찍을게요" },
-        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" }
+        { cn: "你扫我", py: "nǐ sǎo wǒ", kr_read: "니 사오 워", kr: "제 QR 찍어주세요" },
+        { cn: "可以付现金吗?", py: "kěyǐ fù xiànjīn ma?", kr_read: "커이 푸 시앤진 마?", kr: "현금 결제 되나요?" }
+    ],
+    "긴급/도움": [
+        { cn: "请帮帮我", py: "qǐng bāngbang wǒ", kr_read: "칭 방방 워", kr: "좀 도와주세요" },
+        { cn: "医院", py: "yīyuàn", kr_read: "이위앤", kr: "병원" },
+        { cn: "派出所", py: "pàichūsuǒ", kr_read: "파이추쑤오", kr: "경찰서" },
+        { cn: "药店在哪里?", py: "yàodiàn zài nǎlǐ?", kr_read: "야오띠앤 짜이 나리?", kr: "약국이 어디예요?" }
     ]
 };
