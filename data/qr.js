5f50cd113df5a51aecf5cedeb80f82c850b75b7b
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-31 08:49:05 +0900

    Update QR code image sources in qr.js

--- data/qr.js
+++ data/qr.js
@@ -3,10 +3,10 @@ window.qrData = [
         kr: '입국신고서 QR', 
         cn: '外国人入境卡',
         qrs: [
-            { name: '박종현', src: 'images/qr_disney_1.png' },
-            { name: '한재순', src: 'images/qr_disney_2.png' },
-            { name: '박윤희', src: 'images/qr_disney_3.png' },
-            { name: '박현수', src: 'images/qr_disney_4.png' }
+            { name: '박종현', src: 'images/qr_1.png' },
+            { name: '한재순', src: 'images/qr_2.png' },
+            { name: '박윤희', src: 'images/qr_3.png' },
+            { name: '박현수', src: 'images/qr_4.png' }
         ]
     }, 
     { 
