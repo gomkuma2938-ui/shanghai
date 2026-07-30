c748c99202f06e55b21e3466d4f61151b4d3a777
gomkuma2938-ui <gomkuma2938@gmail.com>
2026-07-30 17:36:07 +0900

    Update qr.js

--- data/qr.js
+++ data/qr.js
@@ -8,5 +8,15 @@ window.qrData = [
             { name: '박윤희', src: 'images/qr_disney_3.png' },
             { name: '박현수', src: 'images/qr_disney_4.png' }
         ]
+    }, 
+    { 
+        kr: '예원 입장 QR', 
+        cn: '豫园',
+        qrs: [
+            { name: '박종현', src: 'images/yuyuan_qr_1.png' },
+            { name: '한재순', src: 'images/yuyuan_qr_2.png' },
+            { name: '박윤희', src: 'images/yuyuan_qr_3.png' },
+            { name: '박현수', src: 'images/yuyuan_qr_4.png' }
+        ]
     }  
 ];
